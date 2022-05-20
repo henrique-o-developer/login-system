bd389baad422ea37d664c19c443ff693caa98fc8
henrique-o-developer <henriquefranchescorosario@gmail.com>
2022-05-20 20:17:33 -0300

    version: 67

--- pages/api/user/singup.js
+++ pages/api/user/singup.js
@@ -5,7 +5,7 @@ var database = require("../../../gedf");
 var fs = require("fs")
 
 export default async function (req, res) {
-	res.status(200).send(__dirname)
+	return res.status(200).send(__dirname)
 
 	/*try {
 		req.query
